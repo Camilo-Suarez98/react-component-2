1c06f1a1467ffcfec0dd127e78d068535b0d4b9b
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-07-01 12:10:17 -0500

    Styles desing

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import './Card.scss'
+import { RatingRange } from '../RatingRange'
 
 export const Card = () => {
   return (
-    <div>
-      <h2>Hello World</h2>
+    <div className='card-rating'>
+      <span className="icon">
+        <img src="../../../public/icon-star.svg" alt="test" />
+      </span>
+      <div className="text">
+        <h2>How did we do?</h2>
+        <p>Please let us know how we did with your support request All feedback is appreciatedto help us to improve our offering</p>
+      </div>
+      <form className="rating-buttons">
+        <RatingRange />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
